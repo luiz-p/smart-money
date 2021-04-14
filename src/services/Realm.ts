68f434b173f61bdde66c0dac41ae730caec3729a
Luiz Pires <lpiresjunior@gmail.com>
2021-04-14 06:49:59 -0300

    add initDB & dropDB methods

--- src/services/Realm.ts
+++ src/services/Realm.ts
@@ -1,13 +1,50 @@
 import Realm from 'realm';
 
+import {getDefaultCategories} from './Categories';
+
 import CategorySchema from '../schemas/CategorySchema';
 import EntrySchema from '../schemas/EntrySchema';
 
 export const getRealm = async () => {
   const realm = await Realm.open({
     schema: [CategorySchema, EntrySchema],
-    schemaVersion: 1,
+    schemaVersion: 2,
   });
 
+  // dropDB(realm);
+  initDB(realm);
+
   return realm;
+};
+
+export const initDB = (realm: Realm) => {
+  const categoriesLength = realm.objects('Category').length;
+  console.log(`initDB :: categories length: ${categoriesLength}`);
+
+  if (categoriesLength === 0) {
+    const categories = getDefaultCategories();
+
+    console.log('initDB :: initing db...');
+
+    try {
+      realm.write(() => {
+        categories.forEach(category => {
+          console.log(
+            `initDB :: creating category: ${JSON.stringify(category)}`,
+          );
+
+          realm.create('Category', category, Realm.UpdateMode.All);
+        });
+      });
+    } catch (error) {}
+  } else {
+    console.log('initDB :: categories already existing... Skypping.');
+  }
+};
+
+export const dropDB = (realm: Realm) => {
+  console.log('dropDB :: dropping db...');
+  realm.write(() => {
+    realm.deleteAll();
+  });
 };
