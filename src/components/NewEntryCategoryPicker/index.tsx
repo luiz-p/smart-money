553fa6c24e9caec33449306844a2314670921ac3
Luiz Pires <lpiresjunior@gmail.com>
2021-04-14 07:33:18 -0300

    style(NewEntry): modal items

--- src/components/NewEntryCategoryPicker/index.tsx
+++ src/components/NewEntryCategoryPicker/index.tsx
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
-import {Text, TouchableOpacity, View, Modal} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, Modal, Text, TouchableOpacity, View} from 'react-native';
 
+import {getAllCategories} from '../../services/Categories';
 import styles from './style';
 
 const NewEntryCategoryPicker: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
+  const [allCategories, setAllCategories] = useState<
+    Realm.Results<Realm.Object> | []
+  >([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getAllCategories();
+      setAllCategories(data);
+    })();
+  }, []);
 
   return (
     <View>
@@ -15,9 +26,25 @@ const NewEntryCategoryPicker: React.FC = () => {
       </TouchableOpacity>
 
       <Modal animationType="slide" transparent={false} visible={showModal}>
-        <TouchableOpacity onPress={() => setShowModal(false)}>
-          <Text>Fechar</Text>
-        </TouchableOpacity>
+        <View style={styles.modal}>
+          <FlatList
+            data={allCategories}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity style={styles.modalItem}>
+                <Text style={[styles.modalItemText, {color: item.color}]}>
+                  {item.name}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => setShowModal(false)}>
+            <Text style={styles.closeButtonText}>Fechar</Text>
+          </TouchableOpacity>
+        </View>
       </Modal>
     </View>
   );
--- src/components/NewEntryCategoryPicker/style.ts
+++ src/components/NewEntryCategoryPicker/style.ts
@@ -2,6 +2,10 @@ import {StyleSheet} from 'react-native';
 import Colors from '../../utils/Colors';
 
 export default StyleSheet.create({
+  modal: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
   pickerButton: {
     alignItems: 'center',
     backgroundColor: Colors.asphalt,
@@ -13,5 +17,31 @@ export default StyleSheet.create({
   pickerButtonText: {
     fontSize: 28,
     color: Colors.white,
+  },
+  modalItem: {
+    alignItems: 'center',
+    backgroundColor: Colors.asphalt,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 20,
+  },
+  modalItemText: {
+    fontSize: 22,
+    color: Colors.white,
+  },
+  closeButton: {
+    alignItems: 'center',
+    backgroundColor: Colors.background,
+    borderColor: Colors.green,
+    borderWidth: 1,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 10,
+  },
+  closeButtonText: {
+    fontSize: 14,
+    color: Colors.green,
   },
 });
