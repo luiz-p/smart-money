62baf3bf6612bffa64db784de970ca351f09fb76
Luiz Pires <lpiresjunior@gmail.com>
2021-04-18 19:20:52 -0300

    connect balance panel chart with db

--- src/components/BalancePanelChart/index.tsx
+++ src/components/BalancePanelChart/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {LineChart} from 'react-native-svg-charts';
 import Colors from '../../utils/Colors';
+import useBalanceSumByDate from '../../hooks/useBalanceSumByDate';
 
 const BalancePanelChart: React.FC = () => {
-  const data = [100, 80, 30, 120, 10, 35, 100, 10, 100, 10, 100, 10];
+  const [balanceSum] = useBalanceSumByDate();
 
   return (
     <View style={styles.container}>
       <LineChart
         style={styles.chart}
-        data={data}
+        data={balanceSum}
         svg={{stroke: Colors.blueChart}}
-        contentInset={{top: 0, bottom: 20}}>
-        {/* <Grid /> */}
-      </LineChart>
+        contentInset={{top: 0, bottom: 20}}
+      />
     </View>
   );
 };
